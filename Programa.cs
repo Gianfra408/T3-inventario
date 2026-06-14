d257d6339fd1af85f2edb9ef63b95c30a0f4259b
tatianamacalopu4-cell <tatianamacalopu4@gmail.com>
2026-06-14 16:14:06 -0500

    Agrega estructura base del inventario

---
+++ Programa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inventario
+{
+    class Programa
+    {
+        const int MAX_PRODUCTOS = 100;
+
+        static void Main(string[] args)
+        {
+            string[] codigos = new string[MAX_PRODUCTOS];
+            string[] nombres = new string[MAX_PRODUCTOS];
+            double[] precios = new double[MAX_PRODUCTOS];
+            int[] cantidades = new int[MAX_PRODUCTOS];
+            int cantidadProductos = 0;
+
+            Console.WriteLine("Sistema de inventario de productos");
+            Console.WriteLine("Capacidad maxima: " + MAX_PRODUCTOS + " productos");
+        }
+    }
+}
