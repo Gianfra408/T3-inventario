3ac822ee4b2137be2aced26c5fde73a078f923dc
Gianfranco <n00507122@upn.pe>
2026-06-14 10:55:28 -0500

    V1: Inventaqrio inicial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace inventario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Inventário de produtos
+         string[] produtos = new string[5];
+        produtos[0] = "Camiseta";
+        produtos[1] = "Pantalón";
+        produtos[2] = "Zapatos";
+        produtos[3] = "Gorra";
+        produtos[4] = "Bolsa";
+        Console.WriteLine("Inventário de Produtos:");
+        }
+    }
+}
